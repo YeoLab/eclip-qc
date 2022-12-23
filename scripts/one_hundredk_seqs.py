4555aeb868d26f578f957b3753f8bee793f1e0fa
howardxu520 <108168257+howardxu520@users.noreply.github.com>
2022-12-22 17:18:18 -0800

    Update one_hundredk_seqs.py

--- scripts/one_hundredk_seqs.py
+++ scripts/one_hundredk_seqs.py
@@ -10,6 +10,7 @@ sample = os.path.splitext(unmapped_count)[0]
 sample2 = os.path.split(sample)
 bam_file = sample2[1] + ".genome-mapped.bam"
 unmappedCount = int(file1.read())
+sourcePath = file1.read()
 
 
 one_hundredk = 100000
@@ -21,7 +22,7 @@ else:
         one_hundredk_unmapped = one_hundredk/unmappedCount
 
 #output is samtools command
-cmd = "samtools view -f 4 -s " + str(one_hundredk_unmapped) + " /oasis/tscc/scratch/eczhang/larp6/larp6_GRCh38/results/" + bam_file + ">" + out_file
+cmd = "samtools view -f 4 -s " + str(one_hundredk_unmapped) + " " + sourcePath + bam_file + ">" + out_file
 
 print(cmd)
 os.system(cmd)
