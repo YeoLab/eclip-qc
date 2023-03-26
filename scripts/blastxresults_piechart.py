cf169ee1ee8ce9bfb1d09babcec85d623a1d9801
ezhang113 <98048034+ezhang113@users.noreply.github.com>
2023-03-25 18:06:48 -0700

    Update blastxresults_piechart.py

--- scripts/blastxresults_piechart.py
+++ scripts/blastxresults_piechart.py
@@ -17,6 +17,34 @@ piePathSample = os.path.splitext(piePath)[0]
 piePathSample2 = os.path.split(piePathSample)
 pieName = piePathSample2[1]
 
+#ncbi query functions
+def esearch(term, db='gds'):
+    """
+    Queries NCBI using the esearch utility. GEO ('gds') database is used as default for search term.
+    """
+    url = f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db={db}&term={term}&retmax=5000&usehistory=y'
+    response = urllib.request.urlopen(url)
+    return response.read()
+
+
+def get_esummary(esearch_string, db='gds'):
+    """
+    Parses a http response in XML format to obtain the webenv and querykey tokens.
+    Uses NCBI eutils to transform these tokens into web summaries of GEO (db='gds') datasets.
+    """
+    xmldoc = minidom.parseString(esearch_string)
+    try:
+        webenv = xmldoc.getElementsByTagName('WebEnv')[0].firstChild.data
+        querykey = xmldoc.getElementsByTagName('QueryKey')[0].firstChild.data
+        host = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi'
+        params = f'?db={db}&version=2.0&query_key={querykey}&WebEnv={webenv}'
+        url = host + params
+        response = urllib.request.urlopen(url)
+        return response.read()
+    except IndexError as e:
+        print(f"Unparsable publication string ({e}, search={esearch_string}")
+        return ""
+
 #matplotlib to build the piechart
 fig, ax = plt.subplots()
 
@@ -38,7 +66,6 @@ sseqid_np = np.array(sseqid_list)
 df3 = pd.DataFrame(data = sseqid_np, columns=['sseqid'])
 print(df3)
 
-#get the frequency of each blast result
 sseq_count_series = df3['sseqid'].value_counts()
 print(sseq_count_series)
 
@@ -58,25 +85,41 @@ for index,values in sseq_count_series.iteritems():
 #remove these elements from series
 sseq_count_series2 = sseq_count_series.drop(to_remove)
 
+sseq_name_list = []
+sseqid = []
+#replace index sseqids with ncbi name
+for index,values in sseq_count_series2.iteritems():
+        sseqid.append(index)
+        term = str(index)
+        esearch_string = esearch(term=term, db='protein')
+        result = get_esummary(esearch_string=esearch_string, db='protein')
+        result = xmltodict.parse(result)
+        sseq_name = result['eSummaryResult']['DocumentSummarySet']['DocumentSummary']['Title']
+        sseq_name_list.append(sseq_name)
+        #sseq_count_series2.rename(index={index:sseq_name})
+#replace the sseqids with ncbi query names
+replacements = {sseqid:sseq_name_list for sseqid, sseq_name_list in zip(sseqid, sseq_name_list)}
+sseq_count_series3 = sseq_count_series2.rename(replacements)
+#sseq_count_series2 = sseq_count_series2.rename(index=dict(zip(sseq_name_list,sseqid)))
+
 #generate new pandas series with new element to concatenate with old series
 d = {'Other':other_count}
 ser = pd.Series(data=d, index=['Other'])
 
-#append 'Other' element
-sseq_count = sseq_count_series2.append(ser)
+#append new element
+sseq_count = sseq_count_series3.append(ser)
 #print(sseq_count)
 
 #value_counts returns a pandas series so convert to a data frame
 sseq_count_df = pd.DataFrame({'sseqid':sseq_count.index, 'count':sseq_count.values})
 print(sseq_count_df)
 
-#add respective columns
 count = sseq_count_df['count']
 sseq = sseq_count_df['sseqid']
 
 ax.pie(count, labels = sseq, colors=None,autopct='%1.1f%%',startangle=45,
         wedgeprops={"linewidth": 1, "edgecolor": "white"})
 
-plt.title('Blastx Unmapped Sequences Summary ' + pieName)
+plt.title('Blastn Unmapped Sequences Summary ' + pieName)
 plt.show(block=True)
 plt.savefig(piePath,format='png',bbox_inches='tight')
